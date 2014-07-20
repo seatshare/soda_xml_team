789673863f0b9a8bb531997987120032ed15f821
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-07-20 18:00:46 -0500

    Adding some debugging, refactoring *.crt file path

--- lib/soda_xml_team/client.rb
+++ lib/soda_xml_team/client.rb
@@ -5,21 +5,21 @@ module SodaXmlTeam
   
   class Client
     
-    attr_accessor :auth
+    include HTTParty
+    debug_output $stderr
 
     def initialize(username, password)
-      self.auth = {:username => username, :password => password}
+      @auth = {:username => username, :password => password}
+      @certs = 'lib/ca-bundle.crt'
     end
     
     def get_listing(options={})
-      file = 'lib/ca-bundle.crt'
-      response = HTTParty.get(SodaXmlTeam::Address.build("get_listing", options), :basic_auth => self.auth, :ssl_ca_file => file)
+      response = HTTParty.get(SodaXmlTeam::Address.build("get_listing", options), :basic_auth => @auth, :ssl_ca_file => @certs)
       Nokogiri::XML(response.body)
     end
 
     def get_document(options={})
-      file = 'lib/ca-bundle.crt'
-      response = HTTParty.get(SodaXmlTeam::Address.build("get_document", options), :basic_auth => self.auth, :ssl_ca_file => file)
+      response = HTTParty.get(SodaXmlTeam::Address.build("get_document", options), :basic_auth => @auth, :ssl_ca_file => @certs)
       Nokogiri::XML(response.body)
     end
 
