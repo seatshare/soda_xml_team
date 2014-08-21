77374468bb1da7076b9bd75c9e84b77065e71b8c
Brandon Valentine <brandon@brandonvalentine.com>
2014-08-21 17:09:35 -0500

    Resolve SSL certificate verification issues on soda.xmlteam.com
    * Add Mac OS X Mavericks certificate chain
    * Modify Client to pass in absolute path to ssl_ca_file
    * Modify Client to force SSLv3
    * Bump gem version

--- lib/soda_xml_team/client.rb
+++ lib/soda_xml_team/client.rb
@@ -2,23 +2,21 @@ module SodaXmlTeam
 
   require 'httparty'
   require 'nokogiri'
-  
+
   class Client
-    
-    include HTTParty
-    debug_output $stderr
 
     def initialize(username, password)
       @auth = {:username => username, :password => password}
+      @dir = File.dirname __FILE__
     end
-    
+
     def get_listing(options={})
-      response = HTTParty.get(SodaXmlTeam::Address.build("get_listing", options), { :basic_auth => @auth, :verify => false })
+      response = HTTParty.get(SodaXmlTeam::Address.build("get_listing", options), :basic_auth => @auth, :ssl_ca_file => "#{@dir}/../ca-certificates.crt", :ssl_version => :SSLv3)
       Nokogiri::XML(response.body)
     end
 
     def get_document(options={})
-      response = HTTParty.get(SodaXmlTeam::Address.build("get_document", options), { :basic_auth => @auth, :verify => false })
+      response = HTTParty.get(SodaXmlTeam::Address.build("get_document", options), :basic_auth => @auth, :ssl_ca_file => "#{@dir}/../ca-certificates.crt", :ssl_version => :SSLv3)
       Nokogiri::XML(response.body)
     end
 
